ac6ee8e7582a8b5abfca044fb3f4e33e43475f50
rowan <rowand.9231@gmail.com>
2016-11-17 19:38:46 -0600

    Added ramp motor code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptRampMotorSpeed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptRampMotorSpeed.java
@@ -12,10 +12,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
  * This code assumes a DC motor configured with the name "left motor" as is found on a pushbot.
  *
  * INCREMENT sets how much to increase/decrease the power each cycle
- * CYCLE_MS sets the update period.
+ * CYCLE_MS sets the update period.   
  *
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list 
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 @Autonomous(name = "Concept: Ramp Motor Speed", group = "Concept")
 @Disabled
