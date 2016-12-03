6323db8c2b43fb89300d86ad4a9b436c87bba141
daheinz <heinz.david@gmail.com>
2016-12-03 14:45:45 -0600

    Added example of counter usage.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Counters.java
@@ -0,0 +1,112 @@
+/*
+Copyright (c) 2016 Robert Atkinson
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Robert Atkinson nor the names of his contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Drive", group="Linear Opmode")  // @Magic(...) is the other common choice
+
+
+public class Counters extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // Using counters.  Counters can be used to keep track of the number of times something happens
+        // or to cause events to happen or trigger after a condition is met.  The code below will count
+        // the number of times the the loop runs and notify the drive station every time it hits 100.
+
+        // Step 1 define your variable and initialize it to 0
+        int counter = 0;
+
+        // you should also have a variable that determines whether you are using your counter or not.
+        // We use the type boolean for this type of variable.  A boolean can be either true or false.
+        boolean counterOn = true;
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            // in the loop, you'll want to increment the counter every time that you iterate or pass
+            // through the loop.  Java give you an an incrememnter and decrimenter for adding or deleting
+            // one from a counter.  To add a 1 to your counter you would use counter++ and to decrement
+            // your counter you would use counter--.
+
+            // increment the counter only when counterOn == true
+            if (counterOn == true) { counter++; }
+
+            // once you have incremented your counter, you need to do your test on it to determine if
+            // your program need to react.  In this example, you want to show a message on the drive
+            // station when the counter reaches 100.
+
+            if ((counterOn) && (counter == 100))
+            {
+                telemetry.addData("Counter is equal to 100:  ", counter);
+                // at this point, we have satisfied the event we were waiting for.
+
+                // reset your counter
+                counter = 0;
+                // we are done, so turn the counter off.
+                counterOn = false;
+            }
+
+            telemetry.update();
+            idle(); // Always call idle() at the bottom of your while(opModeIsActive()) loop
+        }
+    }
+
+
+}
